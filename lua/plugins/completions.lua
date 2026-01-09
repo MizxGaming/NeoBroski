30477b78a3b98ca0f500aa9b9e1a00f71e7c3c86
MizxGaming <niharbalanayakdudu@gmail.com>
2026-01-09 07:25:55 +0530

    smth

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -2,47 +2,81 @@ return {
 	{
 		"hrsh7th/nvim-cmp",
 		dependencies = {
-			"hrsh7th/cmp-nvim-lsp", -- LSP completion
-			"hrsh7th/cmp-buffer", -- Buffer text completion
-			"hrsh7th/cmp-path", -- File path completion
-			"L3MON4D3/LuaSnip", -- Snippet engine
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"L3MON4D3/LuaSnip",
 			"saadparwaiz1/cmp_luasnip",
 			"rafamadriz/friendly-snippets",
-			"onsails/lspkind.nvim", -- ADD THIS for NvChad-style icons
+			"onsails/lspkind.nvim",
 		},
 		config = function()
 			local cmp = require("cmp")
-			require("luasnip.loaders.from_vscode").lazy_load()
 			local lspkind = require("lspkind")
+			require("luasnip.loaders.from_vscode").lazy_load()
+
 			cmp.setup({
+				-- THIS SECTION FIXES THE AUTOMATIC SELECTION
+				preselect = cmp.PreselectMode.Item,
+				completion = {
+					completeopt = "menu,menuone,noinsert",
+				},
 				snippet = {
 					expand = function(args)
 						require("luasnip").lsp_expand(args.body)
 					end,
 				},
+				window = {
+					completion = {
+						border = "rounded",
+						winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,CursorLine:PmenuSel,Search:None",
+					},
+					documentation = {
+						border = "rounded", -- ADD THIS: Changes the side window to rounded
+						winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,CursorLine:PmenuSel,Search:None",
+					},
+				},
 				formatting = {
+					fields = { "kind", "abbr", "menu" },
 					format = lspkind.cmp_format({
 						mode = "symbol_text",
 						maxwidth = 50,
 						ellipsis_char = "...",
 					}),
 				},
-				window = {
-					completion = cmp.config.window.bordered(), -- Styled popups
-					documentation = cmp.config.window.bordered(),
-				},
+				-- Inside your cmp.setup({...})
 				mapping = cmp.mapping.preset.insert({
-					["<C-k>"] = cmp.mapping.select_prev_item(), -- Navigate menu
+					["<C-k>"] = cmp.mapping.select_prev_item(),
 					["<C-j>"] = cmp.mapping.select_next_item(),
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<CR>"] = cmp.mapping.confirm({ select = true }),
-					["<Tab>"] = cmp.mapping.confirm({ select = true }), -- NvChad feel
+
+					-- NvChad "Super-Tab" behavior
+					["<Tab>"] = cmp.mapping(function(fallback)
+						local luasnip = require("luasnip")
+						if cmp.visible() then
+							cmp.confirm({ select = true }) -- Confirm selection if menu is open
+						elseif luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump() -- Jump to next placeholder (name -> args -> pass)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						local luasnip = require("luasnip")
+						if luasnip.jumpable(-1) then
+							luasnip.jump(-1) -- Jump backwards
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
 				}),
 				sources = cmp.config.sources({
-					{ name = "luasnip" },
-					{ name = "nvim_lsp" },
-					{ name = "buffer" },
-					{ name = "path" },
+					{ name = "luasnip", priority = 1000 },
+					{ name = "nvim_lsp", priority = 750 },
+					{ name = "buffer", priority = 500 },
+					{ name = "path", priority = 250 },
 				}),
 			})
 		end,
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -9,8 +9,24 @@ return {
 				integrations = {
 					barbar = true,
 					cmp = true,
+					gitsigns = true,
+					harpoon = true,
+					-- lualine = true,
+					mason = true,
+					neotree = true,
+					notify = true,
+					nvimtree = true,
+					telescope = true,
+					treesitter = true,
+					which_key = true,
 				},
 				-- transparent_background = true, -- Matching your NvChad preference
+				custom_highlights = function(colors)
+					return {
+						PmenuSel = { bg = colors.blue, fg = colors.base, style = { "bold" } },
+						Pmenu = { bg = colors.base },
+					}
+				end,
 			})
 			vim.cmd.colorscheme("catppuccin")
 		end,
